d4f597e22058bccd87282cb593cee165cdb85e9a
msh31 <180540729+msh31@users.noreply.github.com>
2025-11-25 16:44:10 +0100

    force utf8 encoding on windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 
 #include "tree.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
+
     Tree tree;
 
     std::vector<int> cool_numbers {
-      20, 5, 300, 40, 50, 69, 420
+      10, 20, 5, 30, 40, 50, 69, 420
     };
 
     for (auto number : cool_numbers) {
